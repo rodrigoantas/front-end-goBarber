fdfd1a89ef32be1031451a2f870c8fd34896bb15
Rodrigo Antas <rodr.antas@gmail.com>
2020-12-15 19:42:35 -0300

    dashboard header done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,37 @@
-import * as React from 'react';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiPower, FiClock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
+import { Container, Header, HeaderContent, Profile } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { user, signOut } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo,</span>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 export default Dashboard;
